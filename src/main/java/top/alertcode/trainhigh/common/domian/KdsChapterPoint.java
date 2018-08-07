338ed41096daedce78d79f890f123bacf4a6a53d
RENJING <894331227@qq.com>
2018-08-07 15:39:12 +0800

    init :smiley:

---
+++ src/main/java/top/alertcode/trainhigh/common/domian/KdsChapterPoint.java
@@ -0,0 +1,106 @@
+package top.alertcode.trainhigh.common.domian;
+
+import java.util.Date;
+
+public class KdsChapterPoint {
+
+  private Long id;
+
+  private Long parentId;
+
+  private Long chapterId;
+
+  private String pointNo;
+
+  private String pointName;
+
+  private Integer status;
+
+  private Date createTime;
+
+  private Date updateTime;
+
+  private Byte isDelete;
+
+  private String pointDesc;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getParentId() {
+    return parentId;
+  }
+
+  public void setParentId(Long parentId) {
+    this.parentId = parentId;
+  }
+
+  public Long getChapterId() {
+    return chapterId;
+  }
+
+  public void setChapterId(Long chapterId) {
+    this.chapterId = chapterId;
+  }
+
+  public String getPointNo() {
+    return pointNo;
+  }
+
+  public void setPointNo(String pointNo) {
+    this.pointNo = pointNo == null ? null : pointNo.trim();
+  }
+
+  public String getPointName() {
+    return pointName;
+  }
+
+  public void setPointName(String pointName) {
+    this.pointName = pointName == null ? null : pointName.trim();
+  }
+
+  public Integer getStatus() {
+    return status;
+  }
+
+  public void setStatus(Integer status) {
+    this.status = status;
+  }
+
+  public Date getCreateTime() {
+    return createTime;
+  }
+
+  public void setCreateTime(Date createTime) {
+    this.createTime = createTime;
+  }
+
+  public Date getUpdateTime() {
+    return updateTime;
+  }
+
+  public void setUpdateTime(Date updateTime) {
+    this.updateTime = updateTime;
+  }
+
+  public Byte getIsDelete() {
+    return isDelete;
+  }
+
+  public void setIsDelete(Byte isDelete) {
+    this.isDelete = isDelete;
+  }
+
+  public String getPointDesc() {
+    return pointDesc;
+  }
+
+  public void setPointDesc(String pointDesc) {
+    this.pointDesc = pointDesc == null ? null : pointDesc.trim();
+  }
+}
---
+++ src/main/java/top/alertcode/trainhigh/mapper/KdsChapterPointMapper.java
@@ -0,0 +1,40 @@
+package top.alertcode.trainhigh.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import top.alertcode.trainhigh.common.domian.KdsChapterPoint;
+import top.alertcode.trainhigh.common.domian.KdsChapterPointExample;
+
+public interface KdsChapterPointMapper {
+
+  int countByExample(KdsChapterPointExample example);
+
+  int deleteByExample(KdsChapterPointExample example);
+
+  int deleteByPrimaryKey(Long id);
+
+  int insert(KdsChapterPoint record);
+
+  int insertSelective(KdsChapterPoint record);
+
+  List<KdsChapterPoint> selectByExampleWithBLOBs(KdsChapterPointExample example);
+
+  List<KdsChapterPoint> selectByExample(KdsChapterPointExample example);
+
+  KdsChapterPoint selectByPrimaryKey(Long id);
+
+  int updateByExampleSelective(@Param("record") KdsChapterPoint record,
+      @Param("example") KdsChapterPointExample example);
+
+  int updateByExampleWithBLOBs(@Param("record") KdsChapterPoint record,
+      @Param("example") KdsChapterPointExample example);
+
+  int updateByExample(@Param("record") KdsChapterPoint record,
+      @Param("example") KdsChapterPointExample example);
+
+  int updateByPrimaryKeySelective(KdsChapterPoint record);
+
+  int updateByPrimaryKeyWithBLOBs(KdsChapterPoint record);
+
+  int updateByPrimaryKey(KdsChapterPoint record);
+}
