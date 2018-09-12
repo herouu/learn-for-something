03ac399e2c44a1213f2e9eb99a696359c725c0be
zlao <2269648132@qq.com>
2018-09-12 18:49:59 +0800

    java8 collector optional 方法demo

--- src/main/java/top/alertcode/trainhigh/other/J8Collectors.java
+++ src/main/java/top/alertcode/trainhigh/other/J8Collectors.java
@@ -9,27 +9,58 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.Data;
 
+/**
+ * 请参考https://www.baeldung.com/java-8-collectors
+ */
 public class J8Collectors {
 
+  /**
+   * The entry point of application.
+   *
+   * @param args input arguments
+   */
   public static void main(String[] args) {
-    toList();
-    toSet();
-    toMap();
+    toListCollectors();
+    toSetCollectors();
+    toMapCollectors();
+    joinCollectors();
+    countCollectors();
+    partitioningByCollectors();
+    collectCollectors();
+    collectingAndThen();
   }
 
-  public static void toList() {
+  /**
+   * toList 操作
+   */
+  public static void toListCollectors() {
     List<Integer> collect = Arrays.asList(1, 2, 3, 3, 3, 4, 4).stream()
         .collect(Collectors.toList());
     System.out.println(JSON.toJSONString(collect));
   }
 
-  public static void toSet() {
+  /**
+   * toSet 操作
+   */
+  public static void toSetCollectors() {
     Set<Integer> collect = Arrays.asList(1, 2, 3, 3, 3, 4, 4).stream()
         .collect(Collectors.toSet());
     System.out.println(JSON.toJSONString(collect));
   }
 
-  public static void toMap() {
+  /**
+   * partitioningBy 操作
+   */
+  public static void partitioningByCollectors() {
+    Map<Boolean, List<Integer>> collect = Arrays.asList(1, 2, 3, 3, 3, 4, 4).stream().distinct()
+        .collect(Collectors.partitioningBy(e -> e >= 3));
+    System.out.println(JSON.toJSONString(collect));
+  }
+
+  /**
+   * toMap 操作
+   */
+  public static void toMapCollectors() {
     List<Demo> list = new ArrayList<>();
     list.add(new Demo("1", "1"));
     list.add(new Demo("2", "2"));
@@ -39,15 +70,77 @@ public class J8Collectors {
     System.out.println(JSON.toJSONString(collect));
   }
 
+  /**
+   * joining 操作
+   */
+  public static void joinCollectors() {
+    //只对字符串生效
+    String result = Arrays.asList("1", "2", "3", "4", "5").stream()
+        .collect(Collectors.joining(" ", "prefix ", " suffix"));
+    System.out.println(result);
+  }
+
+  /**
+   * counting 操作
+   */
+  public static void countCollectors() {
+    Long collect = Arrays.asList("1", "2", "3", "4", "5").stream().collect(Collectors.counting());
+    System.out.println(collect);
+  }
+
+  /**
+   * collect 操作
+   */
+  public static void collectCollectors() {
+    ArrayList<Object> collect = Arrays.asList(1, 2, 3, 4, 5, 6, 7).stream()
+        .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+    System.out.println(collect);
+  }
+
+  /**
+   * collectingAndThen 操作.
+   */
+  public static void collectingAndThen() {
+    List<Integer> collect = Arrays.asList(1, 2, 3, 4, 5, 6, 7).stream()
+        .collect(Collectors.collectingAndThen(Collectors.toList(), result -> {
+          result.add(8);
+          return result;
+        }));
+    System.out.println(collect);
+  }
+
+
+  /**
+   * Demo类.
+   *
+   * @author Z佬
+   * @version v1.0
+   * @date 20180912 18:45:40
+   */
   @Data
   static class Demo {
 
+    /**
+     * The Id.
+     */
     private String id;
+    /**
+     * The Name.
+     */
     private String name;
 
+    /**
+     * Instantiates a new Demo.
+     */
     public Demo() {
     }
 
+    /**
+     * Instantiates a new Demo.
+     *
+     * @param id the id
+     * @param name the name
+     */
     public Demo(String id, String name) {
       this.id = id;
       this.name = name;
