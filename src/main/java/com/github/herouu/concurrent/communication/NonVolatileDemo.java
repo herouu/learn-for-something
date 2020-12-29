d709f9da6b35823c09a11de84357aa9a1462887c
herouu <2269648132@qq.com>
2020-12-29 10:02:54 +0800

    volatile使用

--- src/main/java/com/github/herouu/concurrent/communication/NonVolatileDemo.java
+++ src/main/java/com/github/herouu/concurrent/communication/NonVolatileDemo.java
@@ -7,15 +7,22 @@ public class NonVolatileDemo {
 
     public static void main(String[] args) throws Exception {
         Thread thread1 = new Thread(() -> {
-            while (!stop) { //stop=false，不满足停止条件，继续执行
-                //do someting
+            while (true) {
+                if (stop) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("stop=true，满足条件。");
+                    return;
+                }
             }
-            System.out.println("stop=true，满足停止条件。" +
-                    "停止时间：" + System.currentTimeMillis());
+
         });
         thread1.start();
 
-        Thread.sleep(100);//保证主线程修改stop=true，在子线程启动后执行。
+        Thread.sleep(1000);//保证主线程修改stop=true，在子线程启动后执行。
         stop = true; //true
         System.out.println("主线程设置停止标识 stop=true。" +
                 "设置时间：" + System.currentTimeMillis());
